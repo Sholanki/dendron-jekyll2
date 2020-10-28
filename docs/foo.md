ca1b9aea3fefe9a09e44d9f93e29a681cfa52209
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-10-28 20:16:40 +0530

    Add files via upload

---
+++ docs/foo.md
@@ -0,0 +1,12 @@
+---
+id: foo
+sources:
+  - url: "foo.com"
+    name: ""
+---
+
+# Foo
+
+$$
+f(x) = sin(x) 
+$$
---
+++ script/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+# TODO: bump version in just-the-docs.gemspec
+# set new version here
+version=0.3.11
+echo "Packaging gem... \n"
+gem build just-the-docs.gemspec
+git add .
+git commit -m "chore: publish new version"
+git push
+gem push dendron-jekyll-$version.gem
